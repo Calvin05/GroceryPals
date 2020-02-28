593f0dec81295692c081a71f62b1c82a5c1de0d2
Cuong Nguyen <cuongnguyen9505@gmail.com>
2020-02-27 21:42:00 -0500

    move repo

---
+++ GroceryPals/Controllers/ManagerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using GroceryPals.Models;
+
+namespace GroceryPals.Controllers
+{
+    public class ManagerController : Controller
+    {
+		private UserManager<AppUser> userManager;
+		public ManagerController(UserManager<AppUser> usrMgr)
+		{
+			userManager = usrMgr;
+		}
+		public ViewResult Index() => View(userManager.Users);
+	}
+}
---
+++ GroceryPals/Models/SeedData.cs
@@ -0,0 +1,114 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace GroceryPals.Models
+{
+	public class SeedData
+	{
+		public static void EnsurePopulated(IApplicationBuilder app)
+		{
+			ApplicationDbContext context = app.ApplicationServices.GetRequiredService<ApplicationDbContext>();
+
+			context.Database.Migrate();
+
+			if (!context.Products.Any())
+			{
+				context.Products.AddRange(
+					new Product
+					{
+						Name = "Banana",
+						Description = "Organic, Healthy..",
+						Category = "Fruit",
+						Price = 25.99,
+						Year = "2020",
+						FreeShip = "Free Ship"
+					},
+					new Product
+					{
+						Name = "Banana",
+						Description = "Organic, Healthy..",
+						Category = "Fruit",
+						Price = 25.99,
+						Year = "2020",
+						FreeShip = "Free Ship"
+					},
+					new Product
+					{
+						Name = "Banana",
+						Description = "Organic, Healthy..",
+						Category = "Fruit",
+						Price = 25.99,
+						Year = "2020",
+						FreeShip = "Free Ship"
+					},
+					new Product
+					{
+						Name = "Banana",
+						Description = "Organic, Healthy..",
+						Category = "Fruit",
+						Price = 25.99,
+						Year = "2020",
+						FreeShip = "Free Ship"
+					},
+					new Product
+					{
+						Name = "Banana",
+						Description = "Organic, Healthy..",
+						Category = "Fruit",
+						Price = 25.99,
+						Year = "2020",
+						FreeShip = "Free Ship"
+					},
+					new Product
+					{
+						Name = "Banana",
+						Description = "Organic, Healthy..",
+						Category = "Fruit",
+						Price = 25.99,
+						Year = "2020",
+						FreeShip = "Free Ship"
+					},
+					new Product
+					{
+						Name = "Banana",
+						Description = "Organic, Healthy..",
+						Category = "Fruit",
+						Price = 25.99,
+						Year = "2020",
+						FreeShip = "Free Ship"
+					},
+					new Product
+					{
+						Name = "Banana",
+						Description = "Organic, Healthy..",
+						Category = "Fruit",
+						Price = 25.99,
+						Year = "2020",
+						FreeShip = "Free Ship"
+					},
+					new Product
+					{
+						Name = "Banana",
+						Description = "Organic, Healthy..",
+						Category = "Fruit",
+						Price = 25.99,
+						Year = "2020",
+						FreeShip = "Free Ship"
+					},
+					new Product
+					{
+						Name = "Banana",
+						Description = "Organic, Healthy..",
+						Category = "Fruit",
+						Price = 25.99,
+						Year = "2020",
+						FreeShip = "Free Ship"
+					}
+					);
+				context.SaveChanges();
+			}
+		}
+	}
+}
